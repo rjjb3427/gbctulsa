edf01822ef75402bc4a56a57ba60265a47a5f099
Paul Stefan Ort <mail@paulstefanort.com>
2012-09-07 23:26:57 -0500

    Generate Rails 3.2.8 app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gbctulsa::Application.config.secret_token = '65df942010db16eabfaa5ba8bc14a3e52378271771d4f0febc2d988c2c7d73e10ef98f15929b280260ae8562f2b79bb8089fbb15ce4ea72627fc345dbaa44135'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Gbctulsa::Application.config.session_store :cookie_store, key: '_gbctulsa_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Gbctulsa::Application.config.session_store :active_record_store
